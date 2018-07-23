b8fcefb86b76aaa20c423fb52d5e0ed86221329f
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-07-23 11:16:19 +0300

    Create table questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920041245) do
+ActiveRecord::Schema.define(version: 20180723074320) do
 
   create_table "answers", force: :cascade do |t|
     t.text "matter"
